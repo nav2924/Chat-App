b5a3a9e70ac4a95be1162d0caf62ec37c2c6e763
nav2924 <navi29012004@gmail.com>
2024-07-23 14:38:57 +0530

    env removed

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -9,7 +9,7 @@ const protectRoute = async (req, res, next) => {
 			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
 		}
 
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const decoded = jwt.verify(token, 'JNnig1R/pkZxBgTkrzdSGfqSXreok36cMj/l9qkrONI=');
 
 		if (!decoded) {
 			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
