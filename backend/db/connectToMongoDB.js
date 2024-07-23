083b51193bc69aaf8561c75b1f275b69dda128ba
nav2924 <navi29012004@gmail.com>
2024-07-23 14:52:17 +0530

    Deployment v1.0

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect('mongodb+srv://navi29012004:Jimmy29012004@nvn2924.hqfagcg.mongodb.net/?retryWrites=true&w=majority&appName=Nvn2924');
+		await mongoose.connect(process.env.MONGO_DB_URI);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
